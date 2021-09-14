dcbf461066f1dd3a8496762e26e71f41fd28c34f
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-14 07:24:37 +0700

    Update data/ directory

--- gorilla/mux_router.go
+++ gorilla/mux_router.go
@@ -9,37 +9,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// articleHandler handles category and id of the articles.
-func articleHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	_, err := fmt.Fprintf(
-		w,
-		"%v in Category: %v\n",
-		vars["id"],
-		vars["category"],
-	)
-	if err != nil {
-		return
-	}
-}
-
-// detailsHandler handles details of particular articles.
-func detailsHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	_, err := fmt.Fprintf(w, "Details of ID: %v\n", vars["id"])
-	if err != nil {
-		return
-	}
-}
-
-// settingsHandler handles settings of particular articles.
-func settingsHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	_, err := fmt.Fprintf(w, "Settings of ID: %v\n", vars["id"])
-	if err != nil {
-		return
-	}
-}
 
 func main() {
 	var (
