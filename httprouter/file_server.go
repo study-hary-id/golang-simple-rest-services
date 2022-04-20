563e386eb0727773f6d843d44c676096168d0fbc
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-21 04:46:30 +0700

    Update http.Dir on router.ServeFiles

--- httprouter/file_server.go
+++ httprouter/file_server.go
@@ -11,7 +11,7 @@ func main() {
 	router := httprouter.New()
 	router.ServeFiles(
 		// TODO: Replace according to the directory you want to save static files.
-		"/www/*filepath",                   // Endpoint to access the static files.
-		http.Dir("/home/haryansyah/Public/www")) // The location of the file to be served.
+		"/www/*filepath",     // Endpoint to access the static files.
+		http.Dir("../data/")) // The location of the file to be served.
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
